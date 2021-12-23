fcb3ca2703487eb43e8c03d51008596096b3d4d2
Bleda <bleda@mildmania.com>
2021-12-23 04:24:57 +0300

    Final Tweaks of the Day

--- new/js/scrollup.js
+++ new/js/scrollup.js
@@ -6,7 +6,7 @@ jQuery(function() {
 			jQuery('#toTop, #backtotop').fadeOut();
 		}
 	});
-	jQuery('#toTop').click(function() {
+	jQuery('.toTop').click(function() {
 		jQuery('body,html').animate({scrollTop:0},400);
 	});	
 
