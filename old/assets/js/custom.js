8d4ec8b12edcfe14c17efc595e8c694a21f86cc9
Bleda <bleda@mildmania.com>
2021-12-31 16:49:38 +0300

    Old Site Removal

--- old/assets/js/custom.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($) {
-    "use strict";
-
-    $(".blog-post-audio p iframe").css({"width":"100%","height":"100"});
-    $(".blog-post-video").fitVids();
-});
--- old/js/removewatermark.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-    let el = document.querySelector('[alt="www.000webhost.com"]').parentNode.parentNode;
-    el.parentNode.removeChild(el);
-	document.getElementsByTagName("html")[0].style.visibility = "visible";
-}
-
-//<script type="text/javascript" src="js/removewatermark.js"></script>
