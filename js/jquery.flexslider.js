8c3c954978cb2ca2fb0920e52c49bbfd5010098a
Bleda <bledatunay@gmail.com>
2022-01-04 23:52:43 +0300

    Flexslider Navbutton Improvements

--- js/jquery.flexslider.js
+++ js/jquery.flexslider.js
@@ -340,7 +340,7 @@
       },
       directionNav: {
         setup: function() {
-          var directionNavScaffold = $('<ul class="' + namespace + 'direction-nav"><li class="' + namespace + 'nav-prev"><a class="' + namespace + 'prev" href="#">' + slider.vars.prevText + '</a></li><li class="' + namespace + 'nav-next"><a class="' + namespace + 'next" href="#">' + slider.vars.nextText + '</a></li></ul>');
+          var directionNavScaffold = $('<ul class="' + namespace + 'direction-nav"><li class="' + namespace + 'nav-prev"><a class="' + namespace + 'prev" href="#">' + '<i class="fa fa-angle-left flex-but" href="#"></i>' + '</a></li><li class="' + namespace + 'nav-next"><a class="' + namespace + 'next" href="#">' + '<i class="fa fa-angle-right flex-but" href="#"></i>' + '</a></li></ul>');
 
           // CUSTOM DIRECTION NAV:
           if (slider.customDirectionNav) {
