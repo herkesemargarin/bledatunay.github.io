a2275de97d83fa2d50e8ef98ee419c43ad3227d3
Bleda <bledatunay@gmail.com>
2022-01-30 01:33:44 +0300

    Scrollup Tweaks

--- js/scrollup.js
+++ js/scrollup.js
@@ -6,23 +6,20 @@ jQuery(function() {
 			jQuery('#toTop, #backtotop').fadeOut();
 		}
 	});
-	jQuery('.toTop').click(function() {
+	
+	jQuery('.toTopA').click(function() { // absolute
 		jQuery('body,html').animate({scrollTop:0},400);
 	});	
 
-	jQuery('.toTopDet').click(function() {
-		jQuery('body,html').animate({scrollTop:120},800); // scrollTop = Height of the top navigation + some padding
+	jQuery('.toTopF').click(function() { //flex
+		if(jQuery(window).scrollTop() > 120) {
+			jQuery('body,html').animate({scrollTop:120},600); // scrollTop = Height of the top navigation + some padding
+		}	
 	});	
 
-	jQuery('.mobToTop').click(function() {
+	/*jQuery('.mobToTop').click(function() {
 		if ( jQuery(window).width() < 959) {
 			jQuery('body,html').animate({scrollTop:0},400);
 		}
-	});	
-
-	jQuery('.mobToTopDet').click(function() {
-		if ( jQuery(window).width() < 959) {
-			jQuery('body,html').animate({scrollTop:120},800);
-		}
-	});	
+	});*/
 });
